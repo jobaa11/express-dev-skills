7383b75d66fb9382460cf214902d27d800524696
username <youremail@domain.com>
2022-07-12 16:30:28 -0700

    saved changes

---
+++ bin/controllers/skills.js
@@ -0,0 +1,20 @@
+const Skill = require('../models/skill');
+
+module.exports = {
+  index,
+  show
+};
+
+function show(req, res) {
+  // Get the todo for the id that is passed as a route param
+  // All route params are accessed via the req.params object
+  // console.log(req.params)
+  const todo = Skill.getOne(req.params.id);
+  res.render('todos/show', { skill });
+}
+
+function index(req, res) {
+  // Obtain the array of todos from the Todo model
+  const skills = Skill.getAll();
+  res.render('skills/index', { skills });
+}
---
+++ models/skill.js
@@ -0,0 +1,19 @@
+const todos = [
+    {id: 010, skill: 'CSS', aqcuired: true},
+    {id: 224, skill: 'Javascript', aqcuired: true},
+    {id: 464, skill: 'Python', aqcuired: false}
+  ];
+  
+  module.exports = {
+    getAll,
+    getOne
+  };
+  
+  function getOne(id) {
+    id = parseInt(id);
+    return skill.find((skill) => skill.id === id);
+  }
+  
+  function getAll() {
+    return skills;
+  }
