489730ba091f8fb8c2a7da96fb7667ebd1130638
username <youremail@domain.com>
2022-07-12 16:58:52 -0700

    updated model folder

--- routes/skills.js
+++ routes/skills.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const skillsCtrl = require('../controllers/skills');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+// GET /todos (index functionality - list all)
+router.get('/', skillsCtrl.index);
+// GET /todos/:id (show functionality - show one todo)
+router.get('/:id', skillsCtrl.show);
 
 module.exports = router;
